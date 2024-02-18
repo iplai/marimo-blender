c240b9be58a745e2c4e246267b20252dcee3f775
iplai <365141210@qq.com>
2024-02-18 14:56:17 +0800

    Init

--- marimo_blender/__init__.py
+++ marimo_blender/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author": "iplai",
     "description": "Reactive notebook for Python integrated in blender",
     "blender": (2, 80, 0),
-    "version": (0, 2, 1),
+    "version": (0, 2, 2),
     "location": "View 3D > Header Menu > Notebook",
     "doc_url": "https://github.com/iplai/marimo-blender",
     "tracker_url": "https://github.com/iplai/marimo-blender/issues",
